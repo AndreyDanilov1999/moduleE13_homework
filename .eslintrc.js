cd0d3af99b11a316e52f2e3168e736fd58df73cd
AndreyDanilov1999 <danilov.andrey99@mail.ru>
2023-10-19 17:31:53 +0300

    Commit 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,6 @@ module.exports = {
         "semi": ["error", "always"],
 		"quotes": ["error", "double"]
     }
-
+    
 };
     
--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,8 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-document.write("Hello WORLD!")
+document.write("Hello WORLD!");
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbW9kdWxlLWUxMy1ob21ld29yay8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC53cml0ZShcIkhlbGxvIFdPUkxEIVwiKVxyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbW9kdWxlLWUxMy1ob21ld29yay8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC53cml0ZShcIkhlbGxvIFdPUkxEIVwiKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
